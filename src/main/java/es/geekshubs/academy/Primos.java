8c0fbcb33961c8eeac2358714e3d4e2af5d8deb0
DavidVH <60264680+dvaqueroh@users.noreply.github.com>
2021-08-18 09:34:53 +0200

    Actualizo codigo primos.java
    implementacion metodo apply

--- src/main/java/es/geekshubs/academy/Primos.java
+++ src/main/java/es/geekshubs/academy/Primos.java
@@ -12,8 +12,26 @@ public class Primos {
    * @param input
    * @return
    */
-  public List<Integer> apply(final Integer input) {
-    return null;
-  }
+	
 
-}
+public List<Integer> apply(final Integer input) {
+
+	  List primos = new ArrayList();
+	  for (int i = 1; i <= input; i++) {
+		  if(i!=1) {
+			  if (input%i==0) { // comprueba si es primo
+				  //System.out.println(i+" es primo");
+				  primos.add(i);  
+			  }
+		  }
+		  else {
+			  primos.add(null); 
+		  }
+		  
+		
+	  }// fin for
+	  System.out.println(primos);
+	  return primos;
+  }// fin aplly
+
+}// fin clase
